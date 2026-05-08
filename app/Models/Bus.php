860b14c3e7005132e85d86ff8e239f798da5ba29
M. hafizur rahman <m.hafizurrahman@Ms-MacBook-Air.local>
2026-05-08 15:28:21 +0600

    Fixed MassAssignmentException on Bus model

--- app/Models/Bus.php
+++ app/Models/Bus.php
@@ -6,9 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bus extends Model
 {
-    //
     protected $table = 'buses';
 
     public $incrementing = false;
     protected $keyType = 'string';
+
+    protected $fillable = [
+        'id',
+        'capacity',
+        'route_name',
+        'ds_id',
+        'seat_status',
+        'payment_method'
+    ];
 }
